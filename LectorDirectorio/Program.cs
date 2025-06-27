a58a6cfd4b8c492190c4066fe9a59482ba242030
Diego Aredes <aredesdiegofabian@gmail.com>
2025-06-27 16:53:53 -0300

    Agregando mejoras

---
+++ LectorDirectorio/Program.cs
@@ -0,0 +1,33 @@
+﻿
+do
+{
+    Console.WriteLine("Ingrese el path:");
+    string elPath = @"C:\Users\" + Console.ReadLine();
+
+    if (Directory.Exists(elPath))
+    {
+        Console.WriteLine("La carpeta existe."); 
+
+        string[] carpetas = Directory.GetDirectories(elPath);
+
+        foreach (string carpeta in carpetas)
+        {
+            Console.WriteLine(Path.GetFileName(carpeta));
+            var nombreDelArchivo = Directory.GetDirectories(elPath);
+        }
+        break;
+    }
+    else
+        Console.WriteLine("La carpeta no existe, Ingrese una valida:");
+    
+
+} while (true); 
+
+
+
+
+
+//      ..//casita/
+
+
+
